9ea0a7151bbac7d94fd0ca28a12246321beb9549
nishijaiswal <nishijaiswal44@gmail.com>
2019-10-24 10:26:04 +0530

    initial commit

---
+++ Calculator_Nishi/Calculator_Nishi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculator_Nishi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            while(true)
+            {
+                
+
+                Console.WriteLine("Choose:\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit");
+                int ch = int.Parse(Console.ReadLine());
+
+                if (ch == 5)
+                    break;
+
+                Console.WriteLine("Enter the values:");
+                Console.Write("x:");
+                float x = float.Parse(Console.ReadLine());
+                Console.Write("y:");
+                float y = float.Parse(Console.ReadLine());
+
+                
+                if (ch == 1)
+                    Console.WriteLine(x + " + "+y + " = " + (x + y));
+                else if (ch == 2)
+                    Console.WriteLine(x + " - "+y + " = " + (x - y));
+                else if (ch == 3)
+                    Console.WriteLine(x + " * "+y + " = " + (x * y));
+                else if (ch == 4)
+                    Console.WriteLine(x + " / "+y + " = " + (x / y));
+                else
+                    Console.WriteLine("wrong choice.....Try again");
+
+            }
+
+        }
+    }
+}
